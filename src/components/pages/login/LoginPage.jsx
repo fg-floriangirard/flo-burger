c484d90573a1429ac38d421c2886f02d7b4b174d
fg-floriangirard <fg.girardflorian@gmail.com>
2023-12-01 00:09:37 +0100

    💄 F03: LOGO > Add styling and fonts

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -17,19 +17,6 @@ const LoginPageStyled = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  ::before {
-    content: "";
-    background: url("/images/burger-and-fries-background.jpg") rgba(0, 0, 0, 0.7);
-    background-size: cover;
-    background-position: center;
-    background-blend-mode: darken;
-    
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: -1;
-  }
+  background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), 
+              url("/images/burger-and-fries-background.jpg") center/cover;
 `
--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,9 +1,38 @@
 import { styled } from "styled-components";
 
 export default function Logo() {
-  return <LogoStyled>Logo</LogoStyled>
+  return (
+  <LogoStyled>
+    <h1>FLORIAN</h1>
+    <img src="/images/logo-burger-orange.png" alt="logo-florian-burger" />
+    <h1>BURGERS</h1>
+  </LogoStyled>
+  )
 }
 
 const LogoStyled = styled.div`
-  background: blue;
-`;
+  display: flex;
+  align-items: center;
+  transform: scale(2.5);
+
+  h1 {
+    display: inline;
+    text-align: center;
+    color: #ffa01b;
+    font-size: 36px;
+    line-height: 1em;
+    font-weight: 700;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    font-family: "Amatic SC", cursive;
+  }
+
+  img {
+    object-fit: contain;
+    object-position: center;
+    height: 60px;
+    width: 80px;
+    margin: 0 5px;
+  }
+`
+
