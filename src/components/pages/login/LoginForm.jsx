8572a5cde3443ec2edf654a6f6da95efc94fb914
fg-floriangirard <fg.girardflorian@gmail.com>
2023-12-01 00:17:22 +0100

    💄 F03: LOGIN > LoginForm > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -21,7 +21,7 @@ export default function LoginForm() {
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
       <div>
         <h1>Bienvenue chez nous !</h1>
-        <br />
+        <hr />
         <h2>Connectez-vous</h2>
       </div>
       <input 
@@ -36,7 +36,28 @@ export default function LoginForm() {
 }
 
 const LoginFormStyled = styled.div`
-  background: green;
-  display: flex;
-  flex-direction: column;
-`;
+  text-align: center;
+  max-width:500px;
+  min-width: 400px;
+  margin: 0px auto;
+  padding: 2.5rem 2rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
+
+  hr {
+    border: 1.5px solid #f56a2c
+    margin-bottom: 40px;
+  }
+
+  h1 {
+    color: white;
+    font-size: 40px;
+  }
+
+  h2 {
+    color: #8e8b8b;
+    margin: 20px 10px 10px;
+    color: white;
+    font-size: 36px;
+  }
+`
