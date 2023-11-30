6f70713dfb403d5bae7e5c5139fb2431a97dbff7
fg-floriangirard <fg.girardflorian@gmail.com>
2023-11-30 19:20:20 +0100

    ♻️ F01: LOGIN > Create LoginForm

---
+++ src/components/pages/login/LoginForm.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+export default function LoginForm() {
+
+  const [inputValue, setInputValue] = useState("Flo")
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert(`Bonjour ${inputValue}`)
+    setInputValue("")
+  }
+
+  const handleChange = (event) => { 
+    setInputValue(event.target.value)
+  }
+
+  return (
+    <form action="submit" onSubmit={handleSubmit}>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <input 
+        value={inputValue} 
+        onChange={handleChange}
+        type="text" 
+        placeholder="Entrez votre prénom..." 
+        required />
+      <button>Accédez à votre espace</button>
+    </form>
+  )
+}
