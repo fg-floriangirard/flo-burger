afc3c0bf42df528637a050d23d0090e2bd9b3627
fg-floriangirard <fg.girardflorian@gmail.com>
2023-12-01 22:30:07 +0100

    💄 F05: ORDER > MENU > Add final style

--- src/components/pages/order/Navbar/Navbar.jsx
+++ src/components/pages/order/Navbar/Navbar.jsx
@@ -22,6 +22,7 @@ const NavbarStyled = styled.nav`
   padding: 0 20px;
   border-top-left-radius: ${theme.borderRadius.extraRound};
   border-top-right-radius: ${theme.borderRadius.extraRound};
+  border-bottom: 1px solid ${theme.colors.greyLight};
 
   .order-page-logo {
     cursor: pointer;
