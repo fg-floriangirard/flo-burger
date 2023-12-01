2b23f6baea220eb3aa01e880734761bb6731d4c8
fg-floriangirard <fg.girardflorian@gmail.com>
2023-12-01 18:35:16 +0100

    🚧 F04: ORDER > layout > Add container

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,15 +1,33 @@
+import { styled } from "styled-components"
 import { Link, useParams } from "react-router-dom";
+
 
 export default function OrderPage() {
 
   const { username }  = useParams()
 
   return (
-    <div>
-      <h1>Bonjour {username}</h1>
-      <Link to="/"> 
-        <button>Déconnexion</button>
-      </Link>
-    </div>
+    <OrderPageStyled>
+      <div className="container">
+        <h1>Bonjour {username}</h1>
+        <Link to="/">
+          <button>Déconnexion</button>
+        </Link>
+      </div>
+    </OrderPageStyled>
   )
 }
+
+const OrderPageStyled = styled.div`
+  background: orange;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+F
+  .container {
+    background: red;
+    height: 95vh;
+    width: 1400px;
+  }
+`;
